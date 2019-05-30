b49de6da53e7719123b04f5f7f680e726e4210ba
cljmars <cljmars@umich.edu>
2019-05-30 06:59:18 +0000

    Home page part 1 done

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from catalog.models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Genre)
+admin.site.register(Language)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+#admin.site.register(Book)
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+#@admin.register = admin.site.register
+
+
+
+#admin.site.register(Author)
+# Define the author admin class
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+# Register the admin class with the associated model
+
+
+
+#admin.site.register(BookInstance)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book','imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+
+
+
+
+
+
+
+
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -7,7 +7,7 @@ class Genre(models.Model):
     Model representing a book genre (e.g. Science Fiction, Non Fiction).
     """
     name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
-    
+
     def __str__(self):
         """
         String for representing the Model object (in Admin site etc.)
@@ -27,19 +27,27 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.    
+    # Genre class has already been defined so we can specify the object above.
     def __str__(self):
         """
         String for representing the Model object.
         """
         return self.title
-        
+
     def get_absolute_url(self):
         """
         Returns the url to access a particular book instance.
         """
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+    display_genre.short_description = 'Genre'
 
 import uuid # Required for unique book instances
 
@@ -48,7 +56,7 @@ class BookInstance(models.Model):
     Model representing a specific copy of a book (i.e. that can be borrowed from the library).
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
 
@@ -63,7 +71,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ["due_back"]
-        
+
 
     def __str__(self):
         """
@@ -80,16 +88,29 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
-    
+
     def get_absolute_url(self):
         """
         Returns the url to access a particular author instance.
         """
         return reverse('author-detail', args=[str(self.id)])
-    
+
 
     def __str__(self):
         """
         String for representing the Model object.
         """
         return '%s, %s' % (self.last_name, self.first_name)
+
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
+
+
+
+
